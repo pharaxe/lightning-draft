ddb0def501fa932e835a6df4f040f022a714f897
bsweedler <bsweedler@gmail.com>
2017-09-30 06:01:13 +0000

    stats function is working. need to store the stats in a database next.

--- index.php
+++ index.php
@@ -3,25 +3,34 @@
 <html>
 <head>
    <link rel="stylesheet" href="styles.css">
-   <script src="MooTools-Core-1.6.0.js"></script>
-   <script src="MooTools-More-1.6.0.js"></script>
-   <script src="scripts.js"></script>
 </head>
 <body>
 
-<div id="framed-content">
 <?php
  $sheet = "https://docs.google.com/spreadsheets/d/15QCSzgHuwYuWRUvaEduxBhmdY1XCBE93gGpK2pC6YFo/edit#gid=0";
- $cube = "http://www.cubetutor.com/viewcube/5936";
+ $cube = "http://www.cubetutor.com/viewcube/2432";
 
+
+echo '<div id="links">';
+echo '<a href="'.$sheet.'">Link to spreadsheet.</a>';
+echo '<a id="cubelink" href="'.$cube.'">Link to cube.</a>';
+echo '</div>';
+
+
+echo '<div id="framed-content">';
 
 echo '<iframe class="tab" id="sheets" src="' . $sheet . '"> </iframe>';
-echo '<iframe class="tab" id="cube" src="' . $cube . '"> </iframe>';
+echo '<iframe onload="cubeLoaded(this)" class="tab" id="cube" src="' . $cube . '"> </iframe>';
 
+
+echo '<section id="test" data-test="hello world"> </section>'
 ?>
 
 </div>
 <div id="clear"></div>
 
 </body>
+<script src="MooTools-Core-1.6.0.js"></script>
+<script src="MooTools-More-1.6.0.js"></script>
+<script src="scripts.js"></script>
 </html>
