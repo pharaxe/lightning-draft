a9c68eb77652edf79d9715b416250d713497aa30
bsweedler <bsweedler@gmail.com>
2018-01-06 17:16:46 -0600

    function to get list of picks

--- src/AppBundle/Controller/DraftController.php
+++ src/AppBundle/Controller/DraftController.php
@@ -21,6 +21,9 @@ class DraftController extends Controller
        $multiverseid = $request->query->get('card');
        $playerid = $request->query->get('player');
 
+       if ($multiverseid == null) {
+          return $this->getPicks($playerid);
+       }
 
        $art = $this->getDoctrine()
           ->getRepository(Art::class)
@@ -45,6 +48,30 @@ class DraftController extends Controller
        $ret[] = array(
           'response' => "hello world",
           'mid' => $art->getMultiverseid()
+       );
+
+       $response = new JsonResponse($ret);
+
+       $response->headers->set('Access-Control-Allow-Origin', '*');
+
+       return $response;
+    }
+
+    public function getPicks($playerid) {
+       $player = $this->getDoctrine()
+          ->getRepository(Player::class)
+          ->find($playerid);
+
+       $picks = $player->getPool()->getPicks();
+
+       $pickIds = array();
+       foreach ($picks as $pick) {
+          $pickIds[] = $pick->getArt()->getMultiverseid();
+       }
+
+       $ret[] = array(
+          'player' => $playerid,
+          'picks' => $pickIds
        );
 
        $response = new JsonResponse($ret);
